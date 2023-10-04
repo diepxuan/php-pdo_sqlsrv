a8eba3e2e90afa549b6874d466ee48ce92339df0
Tran Ngoc Duc <caothu91@gmail.com>
2023-10-05 00:53:36 +0700

    output file are moving

--- ci/move_packages.sh
+++ ci/move_packages.sh
@@ -4,19 +4,20 @@
 set -e
 # set -u
 . $(dirname $(realpath "$BASH_SOURCE"))/head.sh
+regex='^php.*(.deb|.ddeb|.buildinfo|.changes|.dsc|.tar.xz|.tar.gz|.tar.[[:alpha:]]+)$'
 
 mkdir -p $dists_dir
-
-regex='^php.*(.deb|.ddeb|.buildinfo|.changes|.dsc|.tar.xz|.tar.gz|.tar.[[:alpha:]]+)$'
+ls $source_dir | grep -E $regex
+ls $pwd_dir | grep -E $regex
 
 while read -r file; do
     [[ -f "$source_dir/$file" ]] &&
-        # [[ ! -f "$dists_dir/$file" ]] &&
+        && echo "move $source_dir/$file to $dists_dir/"
         cp -urf "$source_dir/$file" "$dists_dir/" || true
 done < <(ls $source_dir/ | grep -E $regex)
 
 while read -r file; do
     [[ -f "$pwd_dir/$file" ]] &&
-        # [[ ! -f "$dists_dir/$file" ]] &&
+        && echo "move $pwd_dir/$file to $dists_dir/"
         cp -urf "$pwd_dir/$file" "$dists_dir/" || true
 done < <(ls $pwd_dir/ | grep -E $regex)
