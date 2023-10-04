7ae0b5ef2e0a907b160440948f49ee66a924f883
Tran Ngoc Duc <caothu91@gmail.com>
2023-10-04 16:31:06 +0700

    create default dput config

---
+++ ci/put_ppa_packages.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#!/bin/bash
+
+set -e
+# set -u
+. $(dirname $(realpath "$BASH_SOURCE"))/head.sh
+
+cat | tee ~/.dput.cf <<-EOF
+[caothu91ppa]
+fqdn = ppa.launchpad.net
+method = ftp
+incoming = ~caothu91/ubuntu/ppa/
+login = anonymous
+allow_unsigned_uploads = 0
+EOF
+
+__package=dists/$(ls dists/ | grep $(dpkg-scansources dists/ | grep .dsc | head -n 1 | awk '{print $3}' | sed 's|.dsc||g') | grep _source.changes)
+dput caothu91ppa $__package
