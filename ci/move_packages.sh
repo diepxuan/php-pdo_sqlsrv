e041150a7968ab1870d6a0f16e080f45ba6cb199
Tran Ngoc Duc <caothu91@gmail.com>
2023-10-05 14:42:17 +0700

    ISSUES: mv: invalid option -- 'r'

--- ci/move_packages.sh
+++ ci/move_packages.sh
@@ -9,9 +9,9 @@ regex='^php.*(.deb|.ddeb|.buildinfo|.changes|.dsc|.tar.xz|.tar.gz|.tar.[[:alpha:
 mkdir -p $dists_dir
 
 while read -r file; do
-    mv -vrf "$source_dir/$file" "$dists_dir/" || true
+    mv -vf "$source_dir/$file" "$dists_dir/" || true
 done < <(ls $source_dir/ | grep -E $regex)
 
 while read -r file; do
-    mv -vrf "$pwd_dir/$file" "$dists_dir/" || true
+    mv -vf "$pwd_dir/$file" "$dists_dir/" || true
 done < <(ls $pwd_dir/ | grep -E $regex)
