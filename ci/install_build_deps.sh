c2f6fa88a08fcada6c2f3e38638ef8bcdc8ce4a2
root <root@dev-ubuntu.tail81733.ts.net>
2024-05-03 23:21:35 +0700

    update php multi-version

--- ci/install_build_deps.sh
+++ ci/install_build_deps.sh
@@ -9,7 +9,7 @@ APT_CONF_FILE=/etc/apt/apt.conf.d/50build-deb-action
 
 export DEBIAN_FRONTEND=noninteractive
 
-cat | sudo tee "$APT_CONF_FILE" <<-EOF
+cat | tee "$APT_CONF_FILE" <<-EOF
 APT::Get::Assume-Yes "yes";
 APT::Install-Recommends "no";
 Acquire::Languages "none";
@@ -18,33 +18,33 @@ EOF
 
 # debconf has priority “required” and is indirectly depended on by some
 # essential packages. It is reasonably safe to blindly assume it is installed.
-printf "man-db man-db/auto-update boolean false\n" | sudo debconf-set-selections
+printf "man-db man-db/auto-update boolean false\n" | debconf-set-selections
 
 # add repository for install missing depends
+apt install software-properties-common
 grep -r "/ondrej/php" /etc/apt/sources.list /etc/apt/sources.list.d/*.list >/dev/null 2>&1 ||
-    sudo add-apt-repository ppa:ondrej/php -y
+    add-apt-repository ppa:ondrej/php -y
 
-sudo apt-get update
+apt update
 
 # shellcheck disable=SC2086
-sudo apt-get build-dep $INPUT_APT_OPTS -- "./$INPUT_SOURCE_DIR"
+apt build-dep $INPUT_APT_OPTS -- "./$INPUT_SOURCE_DIR"
 
 # In theory, explicitly installing dpkg-dev would not be necessary. `apt-get
 # build-dep` will *always* install build-essential which depends on dpkg-dev.
 # But let’s be explicit here.
 # shellcheck disable=SC2086
-sudo apt-get install $INPUT_APT_OPTS -- dpkg-dev libdpkg-perl dput $INPUT_EXTRA_BUILD_DEPS
+apt install $INPUT_APT_OPTS -- dpkg-dev libdpkg-perl dput $INPUT_EXTRA_BUILD_DEPS
 
 if ! [[ "18.04 20.04 22.04 23.04" == *"$(lsb_release -rs)"* ]]; then
     echo "Ubuntu $(lsb_release -rs) is not currently supported."
     exit
 fi
 
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
 
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
-
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+apt update
+ACCEPT_EULA=Y apt install -y msodbcsql18
 # optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
+apt install -y unixodbc-dev
