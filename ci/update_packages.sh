d8efc628e6ca4b2129bf187a1f4150fbc956a945
Trần Ngọc Đức <caothu91@gmail.com>
2024-05-04 00:28:59 +0700

    Update update_packages.sh

--- ci/update_packages.sh
+++ ci/update_packages.sh
@@ -13,13 +13,13 @@ ls -la $INPUT_SOURCE_DIR
 
 # Update module sqlsrv release latest
 release_tag=$(echo $package_dist | sed 's|.tgz||g' | cut -d '-' -f2)
-old_release_tag=$(cat $changelog | head -n 1 | awk '{print $2}' | cut -d '+' -f1 | sed 's|[()]||g')
+old_release_tag=$(cat $changelog | head -n 1 | awk '{print $2}' | sed 's|[()]||g')
 sed -i -e "0,/$old_release_tag/ s/$old_release_tag/$release_tag/g" $changelog
 
 # Update os release latest
-old_release_os=$(cat $changelog | head -n 1 | awk '{print $2}' | cut -d '+' -f2 | sed 's|[()]||g')
+# old_release_os=$(cat $changelog | head -n 1 | awk '{print $2}' | cut -d '+' -f2 | sed 's|[()]||g')
 # sed -i -e "0,/$old_release_os/ s/$old_release_os/${DISTRIB}${RELEASE}/g" $changelog
-sed -i -e "0,/$old_release_os/ s/$old_release_os//g" $changelog
+# sed -i -e "0,/$old_release_os/ s/$old_release_os//g" $changelog
 
 # Update os codename
 old_codename_os=$(cat $changelog | head -n 1 | awk '{print $3}')
