80d152c92d441d89ca5415fafebe986f4492e93e
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-03 14:13:50 +0000

    install unixodbc libraries

--- ci/install_build_deps.sh
+++ ci/install_build_deps.sh
@@ -34,3 +34,17 @@ sudo apt-get build-dep $INPUT_APT_OPTS -- "./$INPUT_SOURCE_DIR"
 # But let’s be explicit here.
 # shellcheck disable=SC2086
 sudo apt-get install $INPUT_APT_OPTS -- dpkg-dev libdpkg-perl dput $INPUT_EXTRA_BUILD_DEPS
+
+if ! [[ "18.04 20.04 22.04 23.04" == *"$(lsb_release -rs)"* ]]; then
+    echo "Ubuntu $(lsb_release -rs) is not currently supported."
+    exit
+fi
+
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
