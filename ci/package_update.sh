bbff564868b662392a5ff7b4ee64df05dd6b0dc1
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-04 21:04:51 +0700

    update sed all in file

--- ci/package_update.sh
+++ ci/package_update.sh
@@ -13,10 +13,10 @@ tar xvzf $package_dist -C $source_dir
 
 ls -la $source_dir
 
-sed -i -e "0,/_PROJECT_/ s/_PROJECT_/$project/g" $control
-sed -i -e "0,/_PROJECT_/ s/_PROJECT_/$project/g" $controlin
-sed -i -e "0,/_MODULE_/ s/_MODULE_/$module/g" $control
-sed -i -e "0,/_MODULE_/ s/_MODULE_/$module/g" $controlin
+sed -i -e "s|_PROJECT_|$project|g" $control
+sed -i -e "s|_PROJECT_|$project|g" $controlin
+sed -i -e "s|_MODULE_|$module|g" $control
+sed -i -e "s|_MODULE_|$module|g" $controlin
 
 cat | tee "$source_dir/debian/$module.ini" <<-EOF
 ; configuration for pecl $module module
@@ -28,14 +28,16 @@ release_tag=$(echo $package_dist | sed 's|.tgz||g' | cut -d '-' -f2)
 old_project=$(cat $changelog | head -n 1 | awk '{print $1}' | sed 's|[()]||g')
 old_release_tag=$(cat $changelog | head -n 1 | awk '{print $2}' | sed 's|[()]||g')
 old_codename_os=$(cat $changelog | head -n 1 | awk '{print $3}')
-sed -i -e "0,/$old_project/ s/$old_project/$project/g" $changelog
-sed -i -e "0,/$old_release_tag/ s/$old_release_tag/$release_tag/g" $changelog
-sed -i -e "0,/$old_codename_os/ s/$old_codename_os/$CODENAME;/g" $changelog
-sed -i -e "0,/<$email>  .*/ s/<$email>  .*/<$email>  $timelog/g" $changelog
+
+sed -i -e "s|$old_project|$project|g" $changelog
+sed -i -e "s|$old_release_tag|$release_tag|g" $changelog
+sed -i -e "s|$old_codename_os|$CODENAME|g" $changelog
+sed -i -e "s|<$email>  .*|<$email>  $timelog|g" $changelog
 
 . $ci_dir/package_update.$module.sh
 
 start_group log
 cat $control
 cat $controlin
+cat $changelog
 end_group
