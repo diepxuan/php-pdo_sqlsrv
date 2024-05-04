35890ec2f59f532a555030dac0330ab54b4c34f5
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-04 21:42:30 +0700

    create php config files

--- ci/package_update.sh
+++ ci/package_update.sh
@@ -11,19 +11,29 @@ pecl download $module-$stability
 package_dist=$(ls | grep $module)
 tar xvzf $package_dist -C $source_dir
 
+start_group "view source"
+ls -la ./
 ls -la $source_dir
+ls -la $source_dir/debian
+end_group
 
 sed -i -e "s|_PROJECT_|$project|g" $control
 sed -i -e "s|_PROJECT_|$project|g" $controlin
 sed -i -e "s|_MODULE_|$module|g" $control
 sed -i -e "s|_MODULE_|$module|g" $controlin
 
+start_group "create php config files"
 cat | tee "$source_dir/debian/$module.ini" <<-EOF
 ; configuration for pecl $module module
 ; priority=20
 extension=$module.so
 EOF
+cat | tee "$source_dir/debian/$project.php" <<-EOF
+mod debian/$module.ini
+EOF
+end_group
 
+start_group "update package informations"
 release_tag=$(echo $package_dist | sed 's|.tgz||g' | cut -d '-' -f2)
 old_project=$(cat $changelog | head -n 1 | awk '{print $1}' | sed 's|[()]||g')
 old_release_tag=$(cat $changelog | head -n 1 | awk '{print $2}' | sed 's|[()]||g')
@@ -33,6 +43,7 @@ sed -i -e "s|$old_project|$project|g" $changelog
 sed -i -e "s|$old_release_tag|$release_tag|g" $changelog
 sed -i -e "s|$old_codename_os|$CODENAME|g" $changelog
 sed -i -e "s|<$email>  .*|<$email>  $timelog|g" $changelog
+end_group
 
 . $ci_dir/package_update.$module.sh
 
