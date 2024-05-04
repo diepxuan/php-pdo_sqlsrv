89b2df95b6cb38f6ea3c8eec915eff6f9a4af2cd
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-04 20:57:51 +0700

    add log to debug

--- ci/package_update.sh
+++ ci/package_update.sh
@@ -13,7 +13,6 @@ tar xvzf $package_dist -C $source_dir
 
 ls -la $source_dir
 
-old_project=$(cat $changelog | head -n 1 | awk '{print $1}' | sed 's|[()]||g')
 sed -i -e "0,/_PROJECT_/ s/_PROJECT_/$project/g" $control
 sed -i -e "0,/_PROJECT_/ s/_PROJECT_/$project/g" $controlin
 sed -i -e "0,/_MODULE_/ s/_MODULE_/$module/g" $control
@@ -26,6 +25,7 @@ extension=$module.so
 EOF
 
 release_tag=$(echo $package_dist | sed 's|.tgz||g' | cut -d '-' -f2)
+old_project=$(cat $changelog | head -n 1 | awk '{print $1}' | sed 's|[()]||g')
 old_release_tag=$(cat $changelog | head -n 1 | awk '{print $2}' | sed 's|[()]||g')
 old_codename_os=$(cat $changelog | head -n 1 | awk '{print $3}')
 sed -i -e "0,/$old_project/ s/$old_project/$project/g" $changelog
@@ -34,3 +34,8 @@ sed -i -e "0,/$old_codename_os/ s/$old_codename_os/$CODENAME;/g" $changelog
 sed -i -e "0,/<$email>  .*/ s/<$email>  .*/<$email>  $timelog/g" $changelog
 
 . $ci_dir/package_update.$module.sh
+
+start_group log
+cat $control
+cat $controlin
+end_group
