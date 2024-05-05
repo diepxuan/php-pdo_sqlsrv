baab43c5a2847aba6311d5a20398b5acb66bd375
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-05 12:30:58 +0700

    try to do not use control.in

--- ci/environment.sh
+++ ci/environment.sh
@@ -24,7 +24,7 @@ env pwd_dir $(pwd || dirname $(realpath "$0") || realpath .)
 env email ductn@diepxuan.com
 env changelog $(realpath ./src/debian/changelog)
 env control $(realpath ./src/debian/control)
-env controlin $(realpath ./src/debian/control.in)
+env controlin $(realpath ./src/debian/_control.in.old)
 env rules $(realpath ./src/debian/rules)
 env timelog "$(date -R)"
 
